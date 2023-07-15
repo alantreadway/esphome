732b1127b334d26f73212f0b165f6e9dfc3e73dd
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2023-07-15 22:31:19 +1000

    Checkpoint.

--- esphome/components/canbus_bms/binary_sensor.py
+++ esphome/components/canbus_bms/binary_sensor.py
@@ -1,11 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
+from esphome.const import (
+    DEVICE_CLASS_PROBLEM,
+    ENTITY_CATEGORY_DIAGNOSTIC,
+)
+
 from . import BmsComponent, CONF_BMS_ID, CONF_MSG_ID, CONF_OFFSET, CONF_BIT_NO
 
-CONF_OVER_CURRENT = "over_current"
 
-
+# define an alarm or warning bit, found as a bit in a byte at an offset in a message
 def desc(msg_id, offset, bitno):
     return {
         CONF_MSG_ID: msg_id,
@@ -16,15 +20,40 @@ def desc(msg_id, offset, bitno):
 
 # The sensor map from conf id to message and data decoding information
 TYPES = {
-    CONF_OVER_CURRENT: desc(0x356, 0, 2),
+    "general_alarm": desc(0x35A, 0, 0),
+    "high_voltage": desc(0x35A, 0, 2),
+    "low_voltage": desc(0x35A, 0, 4),
+    "high_temperature": desc(0x35A, 0, 6),
+    "low_temperature": desc(0x35A, 1, 0),
+    "high_temperature_charge": desc(0x35A, 1, 2),
+    "low_temperature_charge": desc(0x35A, 1, 4),
+    "high_current": desc(0x35A, 1, 6),
+    "high_current_charge": desc(0x35A, 2, 0),
+    "contactor_error": desc(0x35A, 2, 2),
+    "short_circuit": desc(0x35A, 2, 4),
+    "bms_internal_error": desc(0x35A, 2, 6),
+    "cell_imbalance": desc(0x35A, 3, 0),
 }
+
+
+def alarm_schema(name):
+    return (
+        cv.Optional(name),
+        binary_sensor.binary_sensor_schema(
+            device_class=DEVICE_CLASS_PROBLEM,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
+    )
+
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(CONF_BMS_ID): cv.use_id(BmsComponent),
         }
-    ).extend(cv.COMPONENT_SCHEMA)
+    )
+    .extend(dict(map(alarm_schema, TYPES)))
+    .extend(cv.COMPONENT_SCHEMA)
 )
 
 
